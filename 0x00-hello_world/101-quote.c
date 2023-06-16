8b3825a25a85e632d8e153aa97ab5b7e4af67a73
Kellymanor <kellymanor92@gmail.com>
2023-06-16 08:28:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@ include <stdio.h>
 include <unistd.h>
 /**
  * main - Entry point
- * Return: Always 0 (Successful)
+ * Return:1 (Successful)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19)\n",stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19)\n",stdout);
 return (1)
 }
